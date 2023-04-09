288fc6012e6d5f0aa7341b1af8ce000673eadc96
Bhuvan-1 <200050040@iitb.ac.in>
2023-04-09 15:29:16 +0530

    cifar10 added

--- data/generate.py
+++ data/generate.py
@@ -1,3 +1,4 @@
+import torch
 import torchvision
 import numpy as np
 import sys
@@ -7,9 +8,18 @@ if(len(sys.argv) != 2):
     print("Usage: python generate.py <dataset_name>")
     sys.exit(1)
 
+device = 'cuda:1' if torch.cuda.is_available() else 'cpu'
+print('device : ',device)
 
 DOWNLOAD_ALL = sys.argv[1] == "all"
 
 
 if(DOWNLOAD_ALL or sys.argv[1] == "cifar10"):
-    torchvision.datasets.CIFAR10(root='./data', train=True, download=True)
+    trainset = torchvision.datasets.CIFAR10(root='./CIFAR10', train=True, download=True)
+    testset = torchvision.datasets.CIFAR10(root='./CIFAR10', train=False, download=True)
+
+    print("CIFAR10 - (train,test) shapes:", trainset.data.shape, testset.data.shape)
+    #CIFAR10 - (train,test) shapes: (50000, 32, 32, 3) (10000, 32, 32, 3)
+
+    np.save("./CIFAR10/CIFAR10_train.npy", trainset.data)
+    np.save("./CIFAR10/CIFAR10_test.npy", testset.data)
--- dataset.py
+++ dataset.py
@@ -1,6 +1,17 @@
 import numpy as np
 from torch.utils.data import Dataset
 
+def seed_torch(seed=0):
+    random.seed(seed)
+    os.environ['PYTHONHASHSEED'] = str(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed) # if you are using multi-GPU.
+    torch.backends.cudnn.benchmark = False
+    torch.backends.cudnn.deterministic = True
+
+
 class ThreeDSinDataset(Dataset):
     def __init__(self, npy_path, mean=None, std=None):
         super().__init__()
@@ -17,6 +28,28 @@ class ThreeDSinDataset(Dataset):
     def __getitem__(self, index):
         return self.data[index]
 
+class CIFAR10(Dataset):
+    def __init__(self, npy_path, mean=None, std=None):
+        super().__init__()
+
+        seed_torch(0)
+
+        self.data = np.load(npy_path)
+        if mean is None:
+            mean = np.mean(self.data, axis=0)
+        if std is None:
+            std = np.std(self.data, axis=0)
+
+        self.mean = mean
+        self.std = std
+        
+        self.data = (self.data - mean) / std
+
+    def __len__(self):
+        return self.data.shape[0]
+
+    def __getitem__(self, index):
+        return self.data[index]
 
 
 
